63e2db6e19bf78c1e470b682fcd2679dfc3e7f32
rungsanle <rungsan.le@gmail.com>
2021-07-03 14:57:31 +0700

    Added basics components to the blog.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ArticleListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
